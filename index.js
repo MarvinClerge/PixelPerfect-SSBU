43e098a27ce72dbb16a48dbc420b18f4ed9813eb
Marvin Clerge <clergemarvin@gmail.com>
2018-11-16 17:55:19 -0500

    comments and active character display

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+// Wait for DOM to load
 document.addEventListener('DOMContentLoaded', function(){
+  // Current highlighted character to display
+  let characterImage = document.querySelector('.player1 img')
+
+  // Find characters box and obtain data from json
   let container = document.querySelector('.characters-container')
   fetch('./data.json')
   .then(response => response.json())
   .then(data => {
 
-
+    // Create a div for each character in the container
     Object.keys(data).forEach(key => {
 
       let div = document.createElement('div')
@@ -13,27 +18,26 @@ document.addEventListener('DOMContentLoaded', function(){
       div.style.backgroundImage = "url(" + data[key]["portrait"] + ")"
       div.id = key
 
+      // Border the show when the div is hovered
       let activeBorder = document.createElement('div')
       activeBorder.className = "active-border"
 
+      // Display border when the div is hovered, set active character
       div.addEventListener('mouseenter', event => {
-        console.log(event.target.id);
         event.target.querySelector('.active-border').style.display = "inline"
+
+        characterImage.src = data[key]["image"]
+        characterImage.alt = key
       })
 
+      // Hide border when the div is not hovered
       div.addEventListener('mouseleave', event => {
-        console.log(event.target.id);
         event.target.querySelector('.active-border').style.display = "none"
       })
 
+      // Add border to div, div to container
       div.appendChild(activeBorder)
       container.appendChild(div)
     })
-
-
-
-
   })
-
-
 })
